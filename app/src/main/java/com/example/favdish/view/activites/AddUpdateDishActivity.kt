303ee11972dd9baccac0b4de010f56bc3dfdbe59
Huseyin Tas <htas767@gmail.com>
2021-07-08 17:09:27 +0300

    [WIP] Reducing the size of the photo taken on the camera

--- app/src/main/java/com/example/favdish/view/activites/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/favdish/view/activites/AddUpdateDishActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Environment
@@ -13,6 +14,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import com.bumptech.glide.Glide
 import com.example.favdish.R
 import com.example.favdish.databinding.ActivityAddUpdateDishBinding
 import com.karumi.dexter.Dexter
@@ -35,7 +37,6 @@ class AddUpdateDishActivity : AppCompatActivity() {
     private val requestCamera = 1
     private val requestMedia = 200
     private lateinit var photoFile: File
-    private val fileName = "photo.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,23 +49,27 @@ class AddUpdateDishActivity : AppCompatActivity() {
         }
     }
 
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == requestCamera) {
-            val image = BitmapFactory.decodeFile(photoFile.absolutePath)
-            binding.ivDishImage.setImageBitmap(image)
-        }
-        if (resultCode == Activity.RESULT_OK && requestCode == requestMedia) {
-            binding.ivDishImage.setImageURI(data?.data)
-        }
-    }
-
     private fun setupActionBar() {
         setSupportActionBar(binding.toolbarAddDishActivity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding.toolbarAddDishActivity.setNavigationOnClickListener {
             onBackPressed()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == requestCamera) {
+            val image = BitmapFactory.decodeFile(photoFile.absolutePath)
+            val imageNew = bitmapDownSizing(image,500)
+            binding.ivDishImage.setImageBitmap(imageNew)
+            //println(imageNew.byteCount)
+        }
+        if (resultCode == Activity.RESULT_OK && requestCode == requestMedia) {
+            //println(data?.data?.let { contentResolver.sizeOfUri(it) })
+            //binding.ivDishImage.setImageURI(data?.data)
+            Glide.with(this).load(data?.data).into(binding.ivDishImage)
+
         }
     }
 
@@ -80,7 +85,6 @@ class AddUpdateDishActivity : AppCompatActivity() {
                         openCamera()
                     }
                 }
-
                 override fun onPermissionRationaleShouldBeShown(
                     permissions: MutableList<PermissionRequest>?,
                     token: PermissionToken?
@@ -101,11 +105,9 @@ class AddUpdateDishActivity : AppCompatActivity() {
                 override fun onPermissionGranted(response: PermissionGrantedResponse?) {
                     openGallery()
                 }
-
                 override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
                     TODO("Not yet implemented")
                 }
-
                 override fun onPermissionRationaleShouldBeShown(
                     permissions: PermissionRequest?,
                     token: PermissionToken?
@@ -126,7 +128,7 @@ class AddUpdateDishActivity : AppCompatActivity() {
 
     private fun openCamera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        photoFile = getPhotoFile(fileName)
+        photoFile = getPhotoFile("IMAGE_")
         val fileProvider =
             FileProvider.getUriForFile(this, "com.example.favdish.view.fileprovider", photoFile)
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
@@ -163,6 +165,23 @@ class AddUpdateDishActivity : AppCompatActivity() {
             dispatchTakePictureGalleryIntent()
             dialog.dismiss()
         }
+    }
+
+    private fun bitmapDownSizing(fileBitmap : Bitmap, maxSize: Int) : Bitmap {
+        var width = fileBitmap.width
+        var height = fileBitmap.height
+
+        val bitmapRate : Double = width.toDouble() /height.toDouble()
+        if (bitmapRate > 1){
+            width = maxSize
+            val reducedSize = width / bitmapRate
+            height = reducedSize.toInt()
+        }else{
+            height = maxSize
+            val shortedSize = height * bitmapRate
+            width = shortedSize.toInt()
+        }
+        return Bitmap.createScaledBitmap(fileBitmap,width,height,true)
     }
 
 }
